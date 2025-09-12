56ac3f29a672762be8cc1eea785ce3114dd4e05e
platanobailando <cjmang@gmail.com>
2025-09-12 18:31:50 -0500

    Fixing gremlins bug

--- src/main/java/ArchipelagoMW/game/locations/patches/CombatRewardScreenPatch.java
+++ src/main/java/ArchipelagoMW/game/locations/patches/CombatRewardScreenPatch.java
@@ -17,55 +17,82 @@ import java.util.Set;
 
 public class CombatRewardScreenPatch {
 
+    private static final void replaceRewards(CombatRewardScreen __instance, ArrayList<RewardItem> ___rewards)
+    {
+        Iterator<RewardItem> rewardItemIterator = ___rewards.iterator();
+        ArrayList<RewardItem> toAdd = new ArrayList<>();
+        LocationTracker locationTracker = APContext.getContext().getLocationTracker();
+        LocationManager locationManager = APContext.getContext().getLocationManager();
+        Set<Long> checkedLocations = new HashSet<>(locationManager.getCheckedLocations());
+        boolean goldSanity = APContext.getContext().getSlotData().goldSanity != 0;
+        boolean potionSanity = APContext.getContext().getSlotData().potionSanity != 0;
+        while (rewardItemIterator.hasNext()) {
+            RewardItem reward = rewardItemIterator.next();
+            NetworkItem item = null;
+            switch (reward.type) {
+                case CARD:
+                    item = locationTracker.sendCardDraw(reward);
+                    break;
+                case RELIC:
+                    item = locationTracker.sendRelic();
+                    break;
+                case GOLD:
+                    if(goldSanity)
+                    {
+                        item = locationTracker.sendGoldReward();
+                    }
+                    break;
+                case POTION:
+                    if(potionSanity)
+                    {
+                        item = locationTracker.sendPotion();
+                    }
+                    break;
+            }
+
+            if (item != null) {
+                rewardItemIterator.remove();
+                // Don't show items already picked up
+                if(!checkedLocations.contains(item.locationID)) {
+                    RewardItem replacementReward = new RewardItem(1);
+                    replacementReward.goldAmt = 0;
+                    replacementReward.text = item.itemName + " NL " + item.playerName;
+                    replacementReward.type = RewardItemPatch.RewardType.ARCHIPELAGO_LOCATION;
+                    toAdd.add(replacementReward);
+                }
+            }
+        }
+        ___rewards.addAll(toAdd);
+    }
+
     @SpirePatch(clz = CombatRewardScreen.class, method = "open", paramtypez = {})
     public static class openPatch {
 
         @SpireInsertPatch(locator = Locator.class, localvars = {})
         public static void Insert(CombatRewardScreen __instance, ArrayList<RewardItem> ___rewards) {
-            Iterator<RewardItem> rewardItemIterator = ___rewards.iterator();
-            ArrayList<RewardItem> toAdd = new ArrayList<>();
-            LocationTracker locationTracker = APContext.getContext().getLocationTracker();
-            LocationManager locationManager = APContext.getContext().getLocationManager();
-            Set<Long> checkedLocations = new HashSet<>(locationManager.getCheckedLocations());
-            boolean goldSanity = APContext.getContext().getSlotData().goldSanity != 0;
-            boolean potionSanity = APContext.getContext().getSlotData().potionSanity != 0;
-            while (rewardItemIterator.hasNext()) {
-                RewardItem reward = rewardItemIterator.next();
-                NetworkItem item = null;
-                switch (reward.type) {
-                    case CARD:
-                        item = locationTracker.sendCardDraw(reward);
-                        break;
-                    case RELIC:
-                        item = locationTracker.sendRelic();
-                        break;
-                    case GOLD:
-                        if(goldSanity)
-                        {
-                            item = locationTracker.sendGoldReward();
-                        }
-                        break;
-                    case POTION:
-                        if(potionSanity)
-                        {
-                            item = locationTracker.sendPotion();
-                        }
-                        break;
-                }
+            replaceRewards(__instance, ___rewards);
+        }
 
-                if (item != null) {
-                    rewardItemIterator.remove();
-                    // Don't show items already picked up
-                    if(!checkedLocations.contains(item.locationID)) {
-                        RewardItem replacementReward = new RewardItem(1);
-                        replacementReward.goldAmt = 0;
-                        replacementReward.text = item.itemName + " NL " + item.playerName;
-                        replacementReward.type = RewardItemPatch.RewardType.ARCHIPELAGO_LOCATION;
-                        toAdd.add(replacementReward);
-                    }
+        private static class Locator extends SpireInsertLocator {
+
+            @Override
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher reward = new Matcher.MethodCallMatcher(CombatRewardScreen.class, "setupItemReward");
+                int[] match = LineFinder.findInOrder(ctBehavior, reward);
+                for (int i = 0; i < match.length; i++) {
+                    match[i]++;
                 }
+                return match;
             }
-            ___rewards.addAll(toAdd);
+        }
+    }
+
+    @SpirePatch(clz = CombatRewardScreen.class, method = "openCombat", paramtypez = {String.class, boolean.class})
+    public static class openCombatPatch {
+
+        @SpireInsertPatch(locator = Locator.class, localvars = {})
+        public static void Insert(CombatRewardScreen __instance, String __label, boolean __smoked, ArrayList<RewardItem> ___rewards) {
+            replaceRewards(__instance, ___rewards);
         }
 
         private static class Locator extends SpireInsertLocator {
